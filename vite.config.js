1501ae2e8cc96a687e12cee5252e629a25049624
oleg998-admin <trussameto@gmail.com>
2025-07-22 16:12:48 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import injectHTML from "vite-plugin-html-inject";
 export default defineConfig(({ command }) => {
   return {
     root: "src",
+    base: "Oleg998/catademy",
     build: {
       sourcemap: true,
       outDir: "../dist",
